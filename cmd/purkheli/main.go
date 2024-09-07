1d87d47c2d9df8d7a91460b3862204dc819fcaa8
Aagaman Luitel <mail@luitelaagaman.com.np>
2024-09-07 17:26:59 +0545

    handlers dont use regex
    how did it work all this time?????

--- cmd/purkheli/main.go
+++ cmd/purkheli/main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	api "github.com/luitel777/purkheli/api"
+	"github.com/luitel777/purkheli/internal/db"
 	server "github.com/luitel777/purkheli/internal/server"
 	"github.com/luitel777/purkheli/internal/template"
 )
@@ -17,17 +18,19 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	// db := db.Database{}
-	// db.InitiateDB()
+	os.Mkdir("uploads", 755)
+
+	db := db.Database{}
+	db.InitiateDB()
 
 	handler := &server.PurkheliHandler{}
 	apihandler := &api.ApiHandler{}
 	templatehandler := &template.TemplateHandler{}
 
 	mux.Handle("/", handler)
-	mux.Handle("/api/*", apihandler)
-	mux.Handle("/posts/*", templatehandler)
-	mux.Handle("/uploads/*", http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
+	mux.Handle("/api/", apihandler)
+	mux.Handle("/posts/", templatehandler)
+	mux.Handle("/uploads/", http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
 	mux.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	serv := &http.Server{
